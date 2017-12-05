469faf47f4ee7003a337b095e9977a78ab5dd921
thakurhimani <himanithakur815@gmail.com>
2017-12-05 20:54:02 +0530

    add validation

--- technical_details.php
+++ technical_details.php
@@ -2,25 +2,26 @@
 	<title>Technical Details</title>
 	<head>
 		<link rel = "stylesheet" type = "text/css" href = "stylesheet.css" />
-		<script src="js_technical_details.js"></script>
+		<script src="technical_details_js.js"></script>
 	</head>                                      
 	<body>
-		<form id="technical_form_id">
+		<form id="technical_form_id" method="POST" action="">
 			<h2 id="heading_form">Technical Details</h2>
 				<fieldset class="fieldSetId">
 					<legend>Interest Area</legend>
 						<table class="technical_table_id">
 							<tr>
 								<td>
-									<input type="checkbox" name="intersetArea" value="development">Development
-									<input type="checkbox" name="intersetArea" value="theming">Theming		
+									<input type="checkbox" name="intersetArea[]" value="development"
+									id="development_id">Development
+									<input type="checkbox" name="intersetArea[]" value="theming" 
+									id="theming_id">Theming	</br>	
+									<input type="checkbox" name="intersetArea[]" value="networking" 
+									id="networking_id">Networking
+									<input type="checkbox" name="intersetArea[]" value="networking"
+									id="system_id">system
 								</td>
-							</tr>
-							<tr>
-								<td>
-									<input type="checkbox" name="intersetArea" value="networking">Networking
-									<input type="checkbox" name="intersetArea" value="networking">Networking
-								</td>
+								<td id="error_intersetArea" class="formError">*</td>
 							</tr>
 						</table>
 					</fieldset>
@@ -29,23 +30,18 @@
 							<table class="technical_table_id">
 								<tr>
 									<td>
-										<input type="checkbox" name="Language" value="C" id="language_c_id">C
-										<input type="checkbox" name="Language" value="C++" 
-											id="language_c++_id">C++
-										<input type="checkbox" name="Language" value="Javascript" 
-											id"javascript_id">Javascript
+										<input type="checkbox" name="Language[]" value="C" id="language_c_id">C
+										<input type="checkbox" name="Language[]" value="C++" 
+											id="language_id">C++
+										<input type="checkbox" name="Language[]" value="Javascript" 
+											id"javascript_id">Javascript </br>
+										<input type="checkbox" name="Language[]" value="php" id="php_id">php
+										<input type="checkbox" name="Language[]" value="HTML" 
+											id="html_id">HTML
+										<input type="checkbox" name="Language[]" value="CSS" id="css_id">CSS
 										</td>
-										<td id="language_id" class="formError">*</td>
+										<td id="error_language_id" class="formError">*</td>
 								</tr>
-									<tr>
-										<td>
-											<input type="checkbox" name="Language" value="php" id="php_id">php
-											<input type="checkbox" name="Language" value="HTML" 
-												id="html_id">HTML
-											<input type="checkbox" name="Language" value="CSS" id="css_id">CSS
-										</td>
-										<td></td>
-									</tr>
 							</table>
 					</fieldset>
 					<fieldset class="fieldSetId">
@@ -53,81 +49,85 @@
 							<table class="technical_table_id">
 								<tr>
 									<td>
-										<input type="checkbox" name="CMSKnowledge" value="Drupal" id="drupal">Drupal
-										<input type="checkbox" name="CMSKnowledge" value="Wordpress" id="wordpress">Wordpress
+										<input type="checkbox" name="CMSKnowledge[]" value="Drupal" 
+										id="drupal_id">Drupal
+										<input type="checkbox" name="CMSKnowledge[]" value="Wordpress" 
+										id="wordpress_id">Wordpress </br>
+										<input type="checkbox" name="CMSKnowledge[]" value="None" id="none_id">None
 									</td>
 									<td id="checkboxError" class="formError">*</td>
 								</tr>
 							</table>
 					</fieldset>
-					<table>
+					<fieldset class="fieldSetId">
+						<legend>Projects</legend>
+							<table>
+								<tr>
+									<td class="project_text">
+										<label>Sr No.</label>
+									</td>
+									<td class="project_text">
+										<label>Project Name</label>
+									</td>
+									<td class="project_text">
+										<label>Technology</label> 
+									</td>
+									<td class="project_text">
+										<label>Role In Project</label>
+									</td>
+								</tr>
+								<tr>
+									<td >
+										<input type="text" name="srNO" class="project_input" 
+										>
+									</td>
+									<td>
+										<input type="text" name="projectName" class="project_input" >
+									</td>
+									<td>
+										<input type="text" name="technology" class="project_input" >
+									</td>
+									<td>
+										<textarea rows="4" cols="50" name="message" id="textarea"  class="project_input"  ></textarea>
+									</td>
+								</tr>
+								<tr>
+									<td >
+										<input type="text" name="srNO" class="project_input" >
+									</td>
+									<td>
+										<input type="text" name="projectName" class="project_input" >
+									</td>
+									<td>
+										<input type="text" name="technology" class="project_input" >
+									</td>
+									<td>
+										<textarea rows="4" cols="50" name="message" id="textarea"  class="project_input" ></textarea>
+									</td>
+								</tr>
+								<tr>
+									<td >
+										<input type="text" name="srNO" class="project_input" >
+									</td>
+									<td>
+										<input type="text" name="projectName" class="project_input" >
+									</td>
+									<td>
+										<input type="text" name="technology" class="project_input" >
+									</td>
+									<td>
+										<textarea rows="4" cols="50" name="message" id="textarea"  class="project_input"  ></textarea>
+									</td>
+								</tr>
+							</table>
+					</fieldset>
+					<table id="submitbtn">
 						<tr>
-					<td id="project_id">
-						<label>Projects</label>
-					</td>
-				</tr>
-						<tr>
-						<td class="project_text">
-							<label>Sr No.</label>
-						</td>
-						<td class="project_text">
-							<label>Project Name</label>
-						</td>
-						<td class="project_text">
-							<label>Technology</label> 
-						</td>
-						<td class="project_text">
-							<label>Role In Project</label>
-						</td>
-					</tr>
-					<tr>
-						<td >
-							<input type="text" name="srNO" class="project_input"/>
-						</td>
-						<td>
-							<input type="text" name="projectName" class="project_input"/>
-						</td>
-						<td>
-							<input type="text" name="technology" class="project_input"/>
-						</td>
-						<td>
-							<textarea rows="4" cols="50" name="message" id="textarea"  class="project_input" ></textarea>
-						</td>
-					</tr>
-					<tr>
-						<td >
-							<input type="text" name="srNO"/ class="project_input">
-						</td>
-						<td>
-							<input type="text" name="projectName" class="project_input"/>
-						</td>
-						<td>
-							<input type="text" name="technology" class="project_input"/>
-						</td>
-						<td>
-							<textarea rows="4" cols="50" name="message" id="textarea"  class="project_input" ></textarea>
-						</td>
-					</tr>
-					<tr>
-						<td >
-							<input type="text" name="srNO"/ class="project_input">
-						</td>
-						<td>
-							<input type="text" name="projectName" class="project_input"/>
-						</td>
-						<td>
-							<input type="text" name="technology" class="project_input"/>
-						</td>
-						<td>
-							<textarea rows="4" cols="50" name="message" id="textarea"  class="project_input" ></textarea>
-						</td>
-					</tr>
-					<tr>
-					<td id="btn_Id" colspan="3">
-					<input id="submitId" type="submit" value="Submit" name="submit" onclick="return validateForm()">
-				</td>
-				</tr>
-						</table>
+							<td>
+								<input id="submitId" type="submit" value="Submit" name="submit" onclick="return validateForm()">
+							</td>
+						</tr>
+					</table>
 		</form>
 	</body>
 </html>
