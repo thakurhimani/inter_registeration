ac7605db1e525222e070f45161e0d58f3786b28f
thakurhimani <himanithakur815@gmail.com>
2017-12-07 11:04:57 +0530

    new academic details form file

--- academic_details_form.php
+++ academic_details_form.php
@@ -1,69 +1,228 @@
 <html>
-	<title>multi page form</title>
+	<title>form</title>
 	<head>
-		<link rel = "stylesheet" type = "text/css" href = "stylesheet.css" />        
-	</head>                                      
+		<link href="table_css.css" rel="stylesheet" type="text/css">
+	</head>
 	<body>
-		<form id="multiple_page_form" action="" method="POST" >
-			<table id="tableid">
-				<tr>
-					<th colspan="3">
-						<h2 id="form_title">Academic Details</h2>
-					</th>
-				</tr>
-				<tr>
-					<td  class="td_text_right">
-						<label>Name Of Degree</label>
-					</td>
-					<td class="td_text">
-						<input type="checkbox" name="degree" value="Graduate">Graduation
-						<input type="checkbox" name="degree" value="Post Graduate">PostGraduation		
-					</td>
-					<td>*</td>
-				</tr>
-				<tr>
-					<td  class="td_text_right">
-						<label> University And College</label>
-					</td>
-					<td>
-						<input type="text" name="college" class="form_input" placeholder="University And College">
-					</td>
-					<td>*</td>
-				</tr>
-				<tr>
-					<td class="td_text_right">
-						<label>Year of passing out</label>
-					</td>
-					<td>
-						<input type="text" name="passingOut"class="form_input"  class="form_input" placeholder="Year of passing out">
-					</td>
-					<td>*</td>
-				</tr>
-				<tr>
-					<td class="td_text_right">
-						<label>Percentage/Grade</label>
-					</td>
-					<td>
-						<input type="text" name="Percentage" class="form_input" placeholder="Percentage/Grade">
-					</td>
-					<td>*</td>
-				</tr>
-				<tr>
-					<td class="td_text_right">
-						<label>Details of any other certification</label>
-					</td>
-					<td>
-						<textarea rows="5" cols="30" name="otherDetails" class="form_input" 
-							placeholder="Other Certification Details"></textarea>
-					</td>
-				</tr>
-				<tr>
-					<td id="btn_Id" colspan="2">
-						<input id="resetId" type="reset" value="Reset">
-						<input id="resetId" type="submit" value="Next" name="submit">
-					</td>
-			</tr>
-			</table>
-		</form>
-	</body>
+		<div class="container">
+			<form id="form_id">
+				<h2 id="heading" > Academic Details</h2> 
+				<fieldset>
+					<legend>Academic Details</legend>
+						<table id="table_id">
+							<tr>
+								<td class="text_feild">
+									<label>Sr.No</label>
+								</td>
+								<td class="text_feild">
+									<label>Name Of Degree</label>
+								</td>
+								<td class="text_feild">
+									<label>Year Of Passing</label>
+								</td>
+								<td class="text_feild">
+									<label>University & College</label>
+								</td>
+								<td class="text_feild">
+									<label> Percentage</label>						
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+							</tr>
+						</table>
+				</fieldset>
+				<fieldset>
+					<legend>Other Certification </legend>
+						<table>
+							<tr>
+								<td class="text_feild">
+									<label>Sr.No</label>
+								</td>
+								<td class="text_feild">
+									<label>Certification</label>
+								</td>
+								<td class="text_feild">
+									<label>Year Of Passing</label>
+								</td>
+								<td class="text_feild">
+									<label>Duration</label>
+								</td>
+								<td class="text_feild">
+									<label> college</label>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+								<td>
+									<input type="text" name="" class="project_work">
+								</td>
+							</tr>
+						</table>
+			</fieldset>
+				<button class="Academic" onclick="my function" click me> <type="button">Next</button>
+			</form>
+		</div>
+</body>
 </html>
